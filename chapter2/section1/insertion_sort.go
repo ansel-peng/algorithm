81720cf71deb1c01b3649f474187885de7b0d98a
pengyang <pengyang0786@163.com>
2021-07-16 21:52:46 +0800

    插入排序倒排

--- chapter2/section1/insertion_sort.go
+++ chapter2/section1/insertion_sort.go
@@ -1,7 +1,7 @@
 package section1
 
 /*
-	插入排序算法
+	插入排序算法(升序)
     unorderedArray为无序数组(切片)
 	现实举例:扑克牌
 */
@@ -14,5 +14,23 @@ func InsertionSort(unorderedArray []int32) {
 			preIndex--
 		}
 		unorderedArray[preIndex+1] = key
+	}
+}
+
+/*
+	2.1-2 重写过程INSERTION-SORT,使之按非升序(而不是非降序)排序
+	插入排序算法(降序)
+	unorderedArray为无序数组(切片)
+*/
+func InsertionSortDesc(unorderedArray []int32) {
+	maxIndex := len(unorderedArray) - 1
+	for i := maxIndex; i >= 0; i-- {
+		postIndex := i + 1
+		key := unorderedArray[i]
+		for postIndex <= maxIndex && unorderedArray[postIndex] > key {
+			unorderedArray[postIndex-1] = unorderedArray[postIndex]
+			postIndex++
+		}
+		unorderedArray[postIndex-1] = key
 	}
 }
--- chapter2/test/algorithm_test.go
+++ chapter2/test/algorithm_test.go
@@ -7,7 +7,12 @@ import (
 )
 
 func TestInsertionSort(t *testing.T) {
-	var array = []int32{3, 3, 5, 7, 10}
+	var array = []int32{3, 45, 5, 7, 10}
+	fmt.Println("-----------升序插入排序---------------")
 	section1.InsertionSort(array)
+	fmt.Println(array)
+	fmt.Println("-----------倒序插入排序---------------")
+	array = []int32{323, 443, 433, 54, 65, 678}
+	section1.InsertionSortDesc(array)
 	fmt.Println(array)
 }
