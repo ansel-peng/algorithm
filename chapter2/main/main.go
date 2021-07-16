9e9092ac648d60fc476ded13b8321ad6f0f8fc19
pengyang <pengyang0786@163.com>
2021-07-16 16:23:27 +0800

    插入排序

--- chapter2/main/main.go
+++ chapter2/main/main.go
@@ -1,12 +1,12 @@
 package main
 
-import "fmt"
-
+import (
+	"algorithm/chapter2/section1"
+	"fmt"
+)
 
 func main() {
-	var example = [5]int32{6, 7, 843, 43, 23}
-	insertionSort(&example)
+	var example = []int32{6, 7, 843, 43, 23}
+	section1.InsertionSort(example)
 	fmt.Println(example)
 }
-
-
--- chapter2/section1/InsertionSort.go
+++ chapter2/section1/InsertionSort.go
@@ -2,15 +2,17 @@ package section1
 
 /*
 	插入排序算法
+    unorderedArray为无序数组(切片)
+	现实举例:扑克牌
 */
-func insertionSort(unorderedArray *[5]int32) {
+func InsertionSort(unorderedArray []int32) {
 	for i := range unorderedArray {
 		preIndex := i - 1
-		current := unorderedArray[i]
-		for preIndex >= 0 && unorderedArray[preIndex] > current {
+		key := unorderedArray[i]
+		for preIndex >= 0 && unorderedArray[preIndex] > key {
 			unorderedArray[preIndex+1] = unorderedArray[preIndex]
-			preIndex -= 1
+			preIndex--
 		}
-		unorderedArray[preIndex+1] = current
+		unorderedArray[preIndex+1] = key
 	}
 }
