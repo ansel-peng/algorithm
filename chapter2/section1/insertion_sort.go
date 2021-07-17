f60ffb14f84693b0d0ad1e2852ef5023aa8f06da
pengyang <pengyang0786@163.com>
2021-07-17 17:26:21 +0800

    fix

--- chapter2/section1/insertion_sort.go
+++ chapter2/section1/insertion_sort.go
@@ -23,14 +23,13 @@ func InsertionSort(unorderedArray []int32) {
 	unorderedArray为无序数组(切片)
 */
 func InsertionSortDesc(unorderedArray []int32) {
-	maxIndex := len(unorderedArray) - 1
-	for i := maxIndex; i >= 0; i-- {
-		postIndex := i + 1
+	for i := range unorderedArray {
+		preIndex := i - 1
 		key := unorderedArray[i]
-		for postIndex <= maxIndex && unorderedArray[postIndex] > key {
-			unorderedArray[postIndex-1] = unorderedArray[postIndex]
-			postIndex++
+		for preIndex >= 0 && unorderedArray[preIndex] < key {
+			unorderedArray[preIndex+1] = unorderedArray[preIndex]
+			preIndex--
 		}
-		unorderedArray[postIndex-1] = key
+		unorderedArray[preIndex+1] = key
 	}
 }
